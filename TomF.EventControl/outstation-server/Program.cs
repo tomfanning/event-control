61563870f3094e3f3f3818773ce68378a22148ec
Tom Fanning <tom@tomfanning.name>
2017-08-27 16:26:16 +0100

    Implement connection indicator, including on ios

--- TomF.EventControl/outstation-server/Program.cs
+++ TomF.EventControl/outstation-server/Program.cs
@@ -8,6 +8,8 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -16,22 +18,52 @@ namespace outstation_server
 {
     public class Program : NancyModule
     {
+        //const string url = "http://localhost:8282";
+        const string url = "http://*:8282";
+
         static void Main(string[] args)
         {
             // start VS as admin to run this
 
-            var server = new NancyHost(new CustomBootstrapper(), new Uri("http://localhost:8282"));
+            var server = new NancyHost(new CustomBootstrapper(), GetUriParams(8282));
             server.Start();
             Console.WriteLine("started!");
             Console.WriteLine("press any key to exit");
 
-            Process.Start("http://localhost:8282");
+            //Process.Start("http://localhost:8282");
             Console.ReadKey();
         }
 
         public Program()
         {
             Get["/testnancy"] = _ => { return "test nancy"; };
+        }
+
+        static Uri[] GetUriParams(int port)
+        {
+            var uriParams = new List<Uri>();
+            string hostName = Dns.GetHostName();
+
+            // Host name URI
+            string hostNameUri = string.Format("http://{0}:{1}", Dns.GetHostName(), port);
+            uriParams.Add(new Uri(hostNameUri));
+
+            // Host address URI(s)
+            var hostEntry = Dns.GetHostEntry(hostName);
+            foreach (var ipAddress in hostEntry.AddressList)
+            {
+                if (ipAddress.AddressFamily == AddressFamily.InterNetwork)  // IPv4 addresses only
+                {
+                    var addrBytes = ipAddress.GetAddressBytes();
+                    string hostAddressUri = string.Format("http://{0}.{1}.{2}.{3}:{4}",
+                    addrBytes[0], addrBytes[1], addrBytes[2], addrBytes[3], port);
+                    uriParams.Add(new Uri(hostAddressUri));
+                }
+            }
+
+            // Localhost URI
+            uriParams.Add(new Uri(string.Format("http://localhost:{0}", port)));
+            return uriParams.ToArray();
         }
     }
 
@@ -52,31 +84,38 @@ namespace outstation_server
             Get["/longpoll/table/{number}"] = parameters => { return TableHasUpdate(parameters); };
         }
 
+        int serverLatestUpdateNum { get { return (int)(DateTime.Now - DateTime.Today).TotalSeconds / 10; } }
+
         private dynamic TableHasUpdate(dynamic parameters)
         {
             int tableNum = int.Parse(parameters.number);
-            int lastUpdateNum = int.Parse(this.Request.Query["last"]);
+            int clientLastUpdateNum = int.Parse(this.Request.Query["last"]);
+
+            if (clientLastUpdateNum > serverLatestUpdateNum)
+            {
+                return new { status = "higherThanIssued" };
+            }
 
             var sw = Stopwatch.StartNew();
 
             while (sw.Elapsed < TimeSpan.FromMinutes(1))
             {
-                if ((int)(DateTime.Now - DateTime.Today).TotalSeconds / 10 > lastUpdateNum)
+                if (serverLatestUpdateNum > clientLastUpdateNum)
                 {
-                    return true;
+                    return new { status = "updateAvailable" };
                 }
 
                 Thread.Sleep(1000);
             }
 
-            return false;
+            return new { status = "nothingNew" };
         }
 
         private dynamic GetTable(dynamic parameters)
         {
             int num = int.Parse(parameters.number);
 
-            var ser = (int)(DateTime.Now - DateTime.Today).TotalSeconds / 10;
+            int ser = serverLatestUpdateNum;
 
             return new PackedData
             {
