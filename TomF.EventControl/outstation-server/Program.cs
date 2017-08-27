3bcdb2cc05eeda721355c88e3da464550d8afd6a
Tom Fanning <tom@tomfanning.name>
2017-08-27 10:09:23 +0100

    Implement ajax including long poll

--- TomF.EventControl/outstation-server/Program.cs
+++ TomF.EventControl/outstation-server/Program.cs
@@ -6,8 +6,10 @@ using Nancy.Hosting.Self;
 using Nancy.Responses;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace outstation_server
@@ -22,6 +24,8 @@ namespace outstation_server
             server.Start();
             Console.WriteLine("started!");
             Console.WriteLine("press any key to exit");
+
+            Process.Start("http://localhost:8282");
             Console.ReadKey();
         }
 
@@ -38,6 +42,59 @@ namespace outstation_server
             nancyConventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddDirectory("/", "Content"));
             base.ConfigureConventions(nancyConventions);
         }
+    }
+
+    public class ApiModule : NancyModule
+    {
+        public ApiModule() : base("/api/v1")
+        {
+            Get["/data/table/{number}"] = parameters => { return GetTable(parameters); };
+            Get["/longpoll/table/{number}"] = parameters => { return TableHasUpdate(parameters); };
+        }
+
+        private dynamic TableHasUpdate(dynamic parameters)
+        {
+            int tableNum = int.Parse(parameters.number);
+            int lastUpdateNum = int.Parse(this.Request.Query["last"]);
+
+            var sw = Stopwatch.StartNew();
+
+            while (sw.Elapsed < TimeSpan.FromMinutes(1))
+            {
+                if ((int)(DateTime.Now - DateTime.Today).TotalSeconds / 10 > lastUpdateNum)
+                {
+                    return true;
+                }
+
+                Thread.Sleep(1000);
+            }
+
+            return false;
+        }
+
+        private dynamic GetTable(dynamic parameters)
+        {
+            int num = int.Parse(parameters.number);
+
+            var ser = (int)(DateTime.Now - DateTime.Today).TotalSeconds / 10;
+
+            return new PackedData
+            {
+                Serial = ser,
+                Headers = new[] { "col1", "col2", "col3" },
+                Data = new string[][]{
+                    new[] { "test1", "test2", "test3" },
+                    new[] { "test4", "test5", "test6 " + ser }
+                }
+            };
+        }
+    }
+
+    public class PackedData
+    {
+        public int Serial { get; set; }
+        public string[] Headers { get; set; }
+        public string[][] Data { get; set; }
     }
 
     public class IndexModule : NancyModule
